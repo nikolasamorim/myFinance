e66c30b6ac6668650d1ded510a349017757d2746
Nikolas Amorim <78495481+nikolasamorim@users.noreply.github.com>
2025-08-06 07:29:23 -0300

    Updated MonthlyChart.tsx

--- src/components/dashboard/MonthlyChart.tsx
+++ src/components/dashboard/MonthlyChart.tsx
@@ -119,35 +119,3 @@ function MonthlyChartGraph({ data }: { data: MonthlyData[] }) {
     </div>
   );
 }
-              </div>
-              <div className="flex space-x-2 h-2">
-                <div
-                  className="bg-green-200 rounded-full"
-                  style={{
-                    width: `${maxValue > 0 ? (month.income / maxValue) * 100 : 0}%`,
-                  }}
-                />
-                <div
-                  className="bg-red-200 rounded-full"
-                  style={{
-                    width: `${maxValue > 0 ? (month.expenses / maxValue) * 100 : 0}%`,
-                  }}
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="flex justify-center space-x-6 mt-6 text-sm">
-          <div className="flex items-center">
-            <div className="w-3 h-3 bg-green-200 rounded-full mr-2" />
-            <span>Receitas</span>
-          </div>
-          <div className="flex items-center">
-            <div className="w-3 h-3 bg-red-200 rounded-full mr-2" />
-            <span>Despesas</span>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  );
-}
