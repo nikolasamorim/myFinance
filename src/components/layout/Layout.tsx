3f9058fbd4540e8c17db8bfcc31a78576e92821b
Nikolas Amorim <nikolas@dzenvolve.com>
2026-02-09 08:35:04 -0300

    Inverte cores e deixa sidebar cinza

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -14,7 +14,7 @@ export function Layout({ children }: LayoutProps) {
       <div className="flex">
         <Sidebar />
         
-        <main className="flex-1 lg:ml-0 min-w-0">
+        <main className="flex-1 bg-white lg:ml-0 min-w-0">
           <div className="p-3 sm:p-4 lg:p-8">
             {children}
           </div>
